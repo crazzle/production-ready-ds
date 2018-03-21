0c66a2c392b0a2783d3f9889c2f2b4d4a674e35c
Mark Keinhörster <mark.keinhoerster@googlemail.com>
2018-03-21 09:18:57 +0100

    Update run_pipeline.sh

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 
-PYTHONPATH='.'
-
-luigi --module 00_training_pipeline TrainModel --version 1 \
-                                               --local-scheduler
+PYTHONPATH='.' luigi --module 00_training_pipeline TrainModel --version 1 \
+                                                              --local-scheduler
 
 PYTHONPATH='.' luigi --module 01_classification_pipeline RangeDailyBase --of Classify \
-                                                         --stop=$(date +"%Y-%m-%d") \
-                                                         --days-back 4 \
-                                                         --Classify-version 1 \
-                                                         --reverse \
-                                                         --local-scheduler
+                                                                        --stop=$(date +"%Y-%m-%d") \
+                                                                        --days-back 4 \
+                                                                        --Classify-version 1 \
+                                                                        --reverse \
+                                                                        --local-scheduler
